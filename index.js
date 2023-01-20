ed2af0533c2ae03857184f51e4bfbd6b2cec4e11
marcelosarinho <marceloactsarinho@gmail.com>
2023-01-20 01:29:37 -0300

    adding challenge resolution

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// function spinImage() {
-//   const sofaDiv = document.querySelector(".sofa-div");
-//   const sofaImage = document.querySelector(".sofa-image");
-//   const stopSpinImage = document.createElement("img");
-//   const spinningSofaImage = document.createElement("img");
-
-//   stopSpinImage.src = "./assets/vectorStroke.png";
-//   stopSpinImage.classList.add("stop-spin-image");
-//   sofaDiv.replaceChild(stopSpinImage, spinButton);
-//   spinningSofaImage.src = "./assets/sofaBoraCodar.gif";
-//   spinningSofaImage.classList.add("spinning-sofa-image");
-//   sofaDiv.replaceChild(spinningSofaImage, sofaImage);
-// }
-
-function spinImage(e) {
-  if(e.target.classList.contains("stop-spin-image")) {
-    spinButton.src = "./assets/vector.png";
-    spinButton.classList.remove("stop-spin-image");
-    sofaImage.classList.remove("spinning-sofa-image");
-    sofaImage.src = "./assets/image2.png";
-  } else {
-    spinButton.src = "./assets/vectorStroke.png";
-    spinButton.classList.add("stop-spin-image");
-    sofaImage.classList.add("spinning-sofa-image");
-    sofaImage.src = "./assets/sofaBoraCodar.gif"
-  }
+function toggle() {
+  document.documentElement.classList.toggle("animated");
 }
-
-const spinButton = document.querySelector(".spin-image");
-spinButton.addEventListener("click", spinImage);
-
-const sofaImage = document.querySelector(".sofa-image");
